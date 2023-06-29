fb386fc24c53458a7c1565c267b19cd555a4d198
sylvianjeru <sylvianjeru63@gmail.com>
2023-06-29 11:46:36 +0300

    submit

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-const titleCased = () => {
-  return tutorials.map((tutorial) => {
-    return tutorial.split(" ").map((word) => word[0].toUpperCase() + word.slice(1)).join(" ");
-  })
-}
-
-console.log(titleCased());
+const totalBatteries = batteryBatches.reduce(function(accumulator, element) {
+    return element + accumulator;
+}, 0)
